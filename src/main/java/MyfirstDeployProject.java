aa7ef9bb873a1430548c8ac40bfe08c5d740619a
nazia <nazia@gmail.com>
2021-08-05 18:19:54 -0500

    Initial commit

---
+++ src/main/java/MyfirstDeployProject.java
@@ -0,0 +1,7 @@
+
+public class MyfirstDeployProject {
+
+	public static void main(String args[]) {
+		System.out.println("This is the Maven project build,store in Git repository and deployed in Jenkins");
+	}
+}
